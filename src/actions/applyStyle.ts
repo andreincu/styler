827db0e1d0a469adbd43c1df4a56ff0cd2f23967
andrei-inc <andrei.iancu@sparktech.ro>
2020-03-25 10:45:18 +0200

    Update applyStyle.ts
    - quick fix

--- src/actions/applyStyle.ts
+++ src/actions/applyStyle.ts
@@ -1,7 +1,9 @@
+import modifiedName from '../utils/modifiedName';
+
 // sync layer style with shared style
 export default (layer, style, styleType, counter) => {
   const layerId = styleType.layer.id;
-  if (style && layer.name === style.name) {
+  if (style && style.name === modifiedName(layer, styleType.affix)) {
     layer[layerId] = style.id;
     counter.applied++;
   } else {
